a85e5066d0432dd37d5951e583a506e61c58ebb9
lufisp <fernando@debian>
2016-11-29 16:53:46 -0500

    New Commit

--- src/main/scala/atmConsumers/AtmConsumer.scala
+++ src/main/scala/atmConsumers/AtmConsumer.scala
@@ -12,6 +12,12 @@ import com.twitter.bijection.avro.GenericAvroCodecs
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 import org.apache.avro.generic.GenericData
+import java.util.Properties
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerRecord
+
+
+
 
 
 /**
@@ -41,28 +47,56 @@ object AtmConsumer {
     
     val directKafkaStream = KafkaUtils.createDirectStream[String, Array[Byte],StringDecoder,DefaultDecoder](ssc,kafkaParams,topic);
     
-   
+//    
+//    directKafkaStream.foreachRDD( rdd =>{
+//      println("New Micro-Batch");
+//      rdd.reduceByKey((a ,b) => ({
+//          val parser = new Schema.Parser();
+//          val schema = parser.parse(USER_SCHEMA);
+//          val recordInjection: Injection[GenericRecord, Array[Byte]] = GenericAvroCodecs.toBinary(schema);          
+//          val recordA:GenericRecord = recordInjection.invert(a).get;
+//          val recordB:GenericRecord = recordInjection.invert(b).get;
+//          val operValueA = Integer.parseInt(recordA.get("operValue").toString());
+//          val operValueB = Integer.parseInt(recordB.get("operValue").toString());          
+//          val avroRecord:GenericData.Record = new GenericData.Record(schema);
+//          avroRecord.put("id", recordA.get("id"));
+//          avroRecord.put("operValue", operValueA + operValueB);          
+//          recordInjection.apply(avroRecord);
+//      })).foreach(avroRecord => {
+//        val parser = new Schema.Parser();
+//        val schema = parser.parse(USER_SCHEMA);
+//        val recordInjection: Injection[GenericRecord, Array[Byte]] = GenericAvroCodecs.toBinary(schema);
+//        val record:GenericRecord = recordInjection.invert(avroRecord._2).get;
+//        System.out.println("Atm_id = " + record.get("id") + ", operValue= " + record.get("operValue"));
+//      })
+//    });
+    
+    
+     
+
     directKafkaStream.foreachRDD( rdd =>{
       println("New Micro-Batch");
-      rdd.reduceByKey((a ,b) => ({
-          val parser = new Schema.Parser();
-          val schema = parser.parse(USER_SCHEMA);
-          val recordInjection: Injection[GenericRecord, Array[Byte]] = GenericAvroCodecs.toBinary(schema);          
-          val recordA:GenericRecord = recordInjection.invert(a).get;
-          val recordB:GenericRecord = recordInjection.invert(b).get;
-          val operValueA = Integer.parseInt(recordA.get("operValue").toString());
-          val operValueB = Integer.parseInt(recordB.get("operValue").toString());          
-          val avroRecord:GenericData.Record = new GenericData.Record(schema);
-          avroRecord.put("id", recordA.get("id"));
-          avroRecord.put("operValue", operValueA + operValueB);          
-          recordInjection.apply(avroRecord);
-      })).foreach(avroRecord => {
+      rdd.map(avroRecord => {
         val parser = new Schema.Parser();
         val schema = parser.parse(USER_SCHEMA);
         val recordInjection: Injection[GenericRecord, Array[Byte]] = GenericAvroCodecs.toBinary(schema);
-        val record:GenericRecord = recordInjection.invert(avroRecord._2).get;
-        System.out.println("Atm_id = " + record.get("id") + ", operValue= " + record.get("operValue"));
+        val record:GenericRecord = recordInjection.invert(avroRecord._2).get;        
+        (record.get("id").toString(),Integer.parseInt(record.get("operValue").toString()))
+        
       })
+      .reduceByKey((a,b) => a + b)
+      .foreachPartition(x => {       
+       val props: Properties = new Properties;
+       props.put("bootstrap.servers", "localhost:9092,localhost:9093");
+       props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+       props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+       val producer : KafkaProducer[String, String] = new KafkaProducer[String, String](props);
+       x.foreach({ row => ({
+        //println("key: " + row._1  + "  value:" + row._2); 
+        val recordToKafka: ProducerRecord[String, String]= new ProducerRecord[String, String]("atmOperationsGrouped", row._1, row._2.toString());
+        producer.send(recordToKafka);
+      })})})
+        
     });
     
     ssc.start();
